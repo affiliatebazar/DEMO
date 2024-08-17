a89bb53bb867789d19cbb89ca15e6c99b907ce85
affiliatebazar <affiliatebazar58@gmail.com>
2024-08-17 13:53:55 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: '217.21.85.1',
-    user: 'jvmm7625_91clubdemo',
-    password: 'jvmm7625_91clubdemo',
-    database: 'jvmm7625_91clubdemo1',
+    user: 'jvmm7625_adcasino',
+    password: 'jvmm7625_adcasino',
+    database: 'jvmm7625_adcasino',
 });
 
 export default connection;
